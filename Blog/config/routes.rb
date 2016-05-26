5dd47844e7fada962ed33f9f6c44f6bf11da40aa
Sambit Acharya <sambitacharya121@gmail.com>
2016-05-26 21:46:28 +0530

    Add new blog application

---
+++ Blog/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/about'
+
+  get 'comments/create'
+
+  resources :posts do
+    resources :comments
+  end
+  root "posts#index"
+
+  get '/about', to: 'pages#about'
+end
